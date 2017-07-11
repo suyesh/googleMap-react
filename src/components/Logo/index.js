82d87d795ca3ef46c3a9e7bbde765709a22fb3dc
Suyesh Bhandari <suyeshb@gmail.com>
2017-07-11 18:37:58 -0400

    somestuff

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -5,7 +5,7 @@ import './Logo.css';
 class Logo extends React.Component {
   render() {
     return (
-      <a href="http://www.nyc.gov/html/dot/html/home/home.shtml" target="_blank"><img src={logo} alt="Main Logo" id="dot-logo"/></a>
+      <a href="https://nycda.com/" target="_blank"><img src={logo} alt="Main Logo" id="dot-logo"/></a>
     );
   }
 }
