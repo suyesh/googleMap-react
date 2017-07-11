6f45ab824853ff27a40e9ec9502b480727767533
Suyesh Bhandari <suyeshb@gmail.com>
2017-07-11 18:33:43 -0400

    Infowindow

--- src/components/Map/infowindow.js
+++ src/components/Map/infowindow.js
@@ -1,6 +1,16 @@
 export default function Infowindow(smo_name, description, size) {
 
-  let content = '<div id="iw-container">' + `<div class="iw-title">${sign_category}</div>` + '<div class="iw-content">' + `<div class="iw-subTitle">${sign_name}</div>` + '<img src="http://maps.marnoto.com/en/5wayscustomizeinfowindow/images/vistalegre.jpg" alt="Porcelain Factory of Vista Alegre" height="115" width="83">' + `<p>${sign_description}</p>` + '<div class="iw-subTitle">Description</div>' + `<p>Size: ${sign_size}<br>` + `<br>Type: ${sign_type}</p>` + '</div>' + '<div class="iw-bottom-gradient"></div>' + '</div>';
+  let content =
+  '<div id="iw-container">' +
+  `<div class="iw-title">${sign_category}</div>` +
+  '<div class="iw-content">' +
+  `<div class="iw-subTitle">${sign_name}</div>` +
+  '<img src="http://maps.marnoto.com/en/5wayscustomizeinfowindow/images/vistalegre.jpg" alt="Porcelain Factory of Vista Alegre" height="115" width="83">' + `<p>${sign_description}</p>` + '<div class="iw-subTitle">Description</div>' +
+  `<p>Size: ${sign_size}<br>` +
+  `<br>Type: ${sign_type}</p>` +
+  '</div>' +
+  '<div class="iw-bottom-gradient"></div>' +
+  '</div>';
 
   return content;
 
